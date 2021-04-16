33ab91391f1461ca94ecb517d0a7c7dfe620093f
sebastian@asimov.cl <Sebastian Vega>
2021-04-16 04:31:03 -0400

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createWebHistory, createRouter } from "vue-router"
+import Home from "../views/Home.vue"
+import Contact from "../views/Contact.vue"
+import Works from "../views/Works.vue"
+import About from "../views/About.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
+  },
+  {
+    path: "/works",
+    name: "Works",
+    component: Works
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
