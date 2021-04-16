e483f95f074aeb9666988b67e50bf026e960a2ea
sebastian@asimov.cl <Sebastian Vega>
2021-04-16 04:56:01 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://sebakc.github.io/portafolio/',
+  base: '/portafolio/',
   plugins: [vue()]
 })
